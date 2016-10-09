ea4fbdaed82ef97b310eeb907f14f548d1546e04
Joseph <joseph.a.gauthier@gmail.com>
2016-10-09 17:47:55 -0500

    Fixed so now the vm can run the jar that comes from clean package, and we can hit a url to get the values

--- src/main/java/test/DBHitter.java
+++ src/main/java/test/DBHitter.java
@@ -17,18 +17,31 @@ public class DBHitter {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:mysql://uaf134404.ddns.uark.edu:3306/TestDB","root", "Capstone");
-			System.out.print("Database is connected !");
+			conn = DriverManager.getConnection("jdbc:mysql://uaf134404.ddns.uark.edu:3306/TestDB", name, "password");
+			Statement stmt = conn.createStatement() ;
+			String query = "select * from DataTable;" ;
+			ResultSet rs = stmt.executeQuery(query) ;
+			String result = "";
+			if(rs.next())
+			{
+				result = rs.getString("value1");
+				conn.close();
+			}
+			else
+			{
+				result = "No results";
+				conn.close();
+			}
+			return result;
 			/*
 			Statement stmt = conn.createStatement() ;
 			String query = "select * from DataTable ;" ;
 			ResultSet rs = stmt.executeQuery(query) ;
 			*/
-			conn.close();
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
+			return e.toString();
 		}
-		return "";
 	}
 }
