3f08b03b7a2d89a7f6ab85d2a5fffe11ff51e3d1
Joseph <joseph.a.gauthier@gmail.com>
2016-11-19 14:11:57 -0600

    push

--- src/main/java/test/DBHitter.java
+++ src/main/java/test/DBHitter.java
@@ -341,6 +341,7 @@ public class DBHitter {
 		int counter = 0;
 		while(rs.next()){
 			packets[counter] = new FriendRequestPacket(fromID, rs.getInt("userID"), rs.getString("username"));
+			counter++;
 		}
 		conn.close();
 		return packets;
